e6ca47aabf0091981bdb7ceb46051f62c1e95e70
leonardo <leonardoblanger@gmail.com>
2018-11-17 00:22:38 -0200

    Temporary fix to handle a bug in the iou method from imgaug.

--- utils/bounding_boxes.py
+++ utils/bounding_boxes.py
@@ -20,6 +20,30 @@ class BoundingBox(ia.BoundingBox):
         self.label = label
         self.confidence = confidence
 
+    # Aparently, there is a bug on the iou method on the pip3 version of imgaug.
+    # So I copied this implementation from the latest version of code on GitHub.
+    def iou(self, other):
+        """
+        Compute the IoU of this bounding box with another one.
+        IoU is the intersection over union, defined as::
+            ``area(intersection(A, B)) / area(union(A, B))``
+            ``= area(intersection(A, B)) / (area(A) + area(B) - area(intersection(A, B)))``
+        Parameters
+        ----------
+        other : imgaug.BoundingBox
+            Other bounding box with which to compare.
+        Returns
+        -------
+        float
+            IoU between the two bounding boxes.
+        """
+        inters = self.intersection(other)
+        if inters is None:
+            return 0.0
+        else:
+            area_union = self.area + other.area - inters.area
+            return inters.area / area_union if area_union > 0 else 0.0
+
     def __repr__(self):
         return self.__str__()
 
