f38f0d84c8afd562dc7c547b927665fb281baabe
leonardo <leonardoblanger@gmail.com>
2018-11-17 00:20:49 -0200

    Temporary fix to handle a bug in the iou method from imgaug.

--- metrics.py
+++ metrics.py
@@ -48,31 +48,23 @@ def AP(batch_ground_truth, batch_predictions, iou_threshold = 0.5):
     total_positives = 0
 
     for ground_truth, predictions in zip(batch_ground_truth, batch_predictions):
-        num_gts = len(ground_truth.bounding_boxes)
-        num_preds = predictions.shape[0]
-        total_positives += num_gts
+        ground_truth = ground_truth.bounding_boxes
+        total_positives += len(ground_truth)
 
-        if num_preds == 0: continue
-        pred_xmin, pred_ymin, pred_xmax, pred_ymax = [predictions[:,i] for i in range(-4,0)]
+        predictions = predictions.bounding_boxes
+        predictions = sorted(predictions, key=lambda x: x.confidence, reverse=True)
 
-        ious = []
-        for gt in ground_truth.bounding_boxes:
-            ious.append(IoU(gt.x1, gt.y1, gt.x2, gt.y2,
-                            pred_xmin, pred_ymin, pred_xmax, pred_ymax))
-        ious = np.array(ious)
+        matched = np.zeros(len(ground_truth))
 
-        matched_preds = []
-        for _ in range(num_gts):
-            best_gt = np.argmax(np.max(ious, axis=1))
-            best_pred = np.argmax(ious[best_gt, :])
+        for pred in predictions:
+            iou = [pred.iou(gt) for gt in ground_truth]
+            i = np.argmax(iou)
 
-            if ious[best_gt, best_pred] >= iou_threshold:
-                matched_preds.append(best_pred)
-                ious[best_gt, :] = -1
-                ious[:, best_pred] = -1
-
-        for p in range(num_preds):
-            all_predictions.append((predictions[p,1], p in matched_preds))
+            if iou[i] >= iou_threshold and not matched[i]:
+                all_predictions.append((pred.confidence, True))
+                matched[i] = True
+            else:
+                all_predictions.append((pred.confidence, False))
 
     all_predictions = sorted(all_predictions, reverse=True)
 
@@ -94,19 +86,22 @@ def AP(batch_ground_truth, batch_predictions, iou_threshold = 0.5):
 
     return np.sum((recalls[1:]-recalls[:-1]) * precisions[1:])
 
-def mAP(ground_truth, predictions, num_classes, background_id = 0, iou_threshold = 0.5):
+def mAP(ground_truth, predictions, classes, iou_threshold = 0.5):
     APs = []
 
-    for label in range(num_classes):
-        if label == background_id: continue
-
+    for label in classes:
         class_ground_truth = [
             ia.BoundingBoxesOnImage(
                 [box for box in boxes.bounding_boxes if box.label == label],
                 shape = boxes.shape
             ) for boxes in ground_truth
         ]
-        class_predictions = [pred[pred[:,0] == label] for pred in predictions]
+        class_predictions = [
+            ia.BoundingBoxesOnImage(
+                [box for box in boxes.bounding_boxes if box.label == label],
+                shape = boxes.shape
+            ) for boxes in predictions
+        ]
 
         APs.append(AP(class_ground_truth, class_predictions, iou_threshold))
 
